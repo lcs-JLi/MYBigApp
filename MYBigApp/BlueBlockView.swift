3e70dc461b4f25593d56909a887de27393e871a7
John Li <jli@lcs.on.ca>
2026-03-01 13:57:53 -0500

    Half done blue block view

---
+++ MYBigApp/BlueBlockView.swift
@@ -0,0 +1,35 @@
+//
+//  BlueBlockView.swift
+//  MYBigApp
+//
+//  Created by Xichen Li on 3/1/26.
+//
+
+
+import SwiftUI
+
+struct BlueBlockView: View {
+            let catagory: String
+            let value: String
+    let description: String
+            
+            var body: some View{
+                VStack{
+                    
+                    
+                    
+                    
+                    Text(catagory)
+                        .foregroundColor(.pink)
+                    Text(value)
+                        .font(.title3)
+                    Text(description)
+                }
+                .padding()
+                .background(Color.blue)
+            }
+            
+            
+            
+            
+        }
--- MYBigApp/ContentView.swift
+++ MYBigApp/ContentView.swift
@@ -11,10 +11,23 @@ struct ContentView: View {
     var body: some View {
         NavigationStack{
             ImformationView(place: "Smith-Ennismore-Lakefield", weather: "6|Cloudy")
+            
+        }
+        VStack{
+            HStack{
+                BlueBlockView(catagory: "Feels like", value: "2", description: "Wind is making it fel colder.")
+                BlueBlockView(catagory: "Feels like", value: "2", description: "Wind is making it fel colder.")
+            }
+            HStack{
+                BlueBlockView(catagory: "Feels like", value: "2", description: "Wind is making it fel colder.")
+                BlueBlockView(catagory: "Feels like", value: "2", description: "Wind is making it fel colder.")
+            }
+            
         }
     }
-    }
-        
+}
+
+
         
         
         
