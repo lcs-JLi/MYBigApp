dbcaa3125b881ed80094163276d6f582b417cfec
John Li <jli@lcs.on.ca>
2026-03-01 13:39:58 -0500

    Finished Navigation Stack

--- MYBigApp/ContentView.swift
+++ MYBigApp/ContentView.swift
@@ -9,16 +9,16 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack{
+            ImformationView(place: "Smith-Ennismore-Lakefield", weather: "6|Cloudy")
         }
-        .padding()
     }
-}
-
-#Preview {
-    ContentView()
-}
+    }
+        
+        
+        
+        
+        #Preview {
+            ContentView()
+        }
+    
---
+++ MYBigApp/ImformationView.swift
@@ -0,0 +1,27 @@
+//
+//  ImformationView.swift
+//  MYBigApp
+//
+//  Created by Xichen Li on 3/1/26.
+//
+
+
+import SwiftUI
+
+struct ImformationView: View {
+            let place: String
+            let weather: String
+            
+            var body: some View{
+                VStack{
+                    Text(place)
+                        .font(.title3)
+                    Text(weather)
+                }
+                
+            }
+            
+            
+            
+            
+        }
