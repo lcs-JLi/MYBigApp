4955254f064d5e8168b722f6d51c9a5ee7aa5b47
John Li <jli@lcs.on.ca>
2026-03-01 13:10:12 -0500

    Initial Commit

---
+++ MYBigApp/MYBigAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MYBigAppApp.swift
+//  MYBigApp
+//
+//  Created by Xichen Li on 3/1/26.
+//
+
+import SwiftUI
+
+@main
+struct MYBigAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
