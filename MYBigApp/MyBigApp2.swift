c3eb7a6ad4afa8b24c0e9be2be231ed14f6fe9c7
John Li <jli@lcs.on.ca>
2026-03-02 11:09:20 -0500

    Second project

---
+++ MYBigApp/MyBigApp2.swift
@@ -0,0 +1,18 @@
+//
+//  MyBigApp2.swift
+//  MYBigApp
+//
+//  Created by Xichen Li on 3/2/26.
+//
+
+import SwiftUI
+
+struct MyBigApp2: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MyBigApp2()
+}
